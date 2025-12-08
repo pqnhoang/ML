4c1e8d76edc7744aee6104ed4d1b16843117973a
pqnhoang <hoang.pqn2123@gmail.com>
2025-12-08 23:45:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -71,6 +71,3 @@ def predict(data: dict):
         "predicted_prob": float(prob),
         "predicted_label": label
     }
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Railway sets PORT
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
